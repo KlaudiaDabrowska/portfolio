84b7f42f90a131df620898fc9883e808a5956f7d
Klaudia Dąbrowska <kosaa96@gmail.com>
2022-06-13 22:27:41 +0200

    feat: add another project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -50,15 +50,28 @@ const Projects: NextPage = () => {
             <div className="card">
               <div className="card-body project-card">
                 <h5 className="card-title text-center fw-bold fs-3 mb-5">
-                  Special title treatment
+                <a
+                    href={"https://murmuring-dawn-23275.herokuapp.com/"}
+                    style={{ textDecoration: "none", color: "black" }}
+                  >
+                    Fitness App
+                  </a>
                 </h5>
                 <p className="card-text text-center">
-                  With supporting text below as a natural lead-in to additional
-                  content.
+                The application allows the user to create his own workout plan. The user can select exercises from the exercise list and can add the number of repetitions. Once he has made his workout plan, he can open it in modal and begin the adventure!
+                <br></br><br></br><br></br><br></br>
                 </p>
-                <a href="#" className="btn btn-primary">
-                  Go somewhere
-                </a>
+                <div className="text-center">
+                  <Image
+                    src={"/img/fitness.png"}
+                    height={600}
+                    width={1000}
+                    alt="coctailbar"
+                  />
+                </div>
+                <p className="card-text text-center text-muted">
+                  Tech stack: Next.JS, Typescript, MUI 
+                </p>
               </div>
             </div>
           </div>
